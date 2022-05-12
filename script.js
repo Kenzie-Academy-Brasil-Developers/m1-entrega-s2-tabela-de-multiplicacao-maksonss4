52e4a6096f7bf913ba367577ce76c4c360eadcb6
maksonss4 <maksonsantoss4@gmail.com>
2022-05-12 11:19:35 -0300

    Adicionei os arquivos HTML e JS

---
+++ script.js
@@ -0,0 +1,26 @@
+function gerarTabela(){
+    let tabela = []
+    for(let l = 0; l < 11; l++){
+        let linha = []
+        for(let c = 0; c < 11; c++){
+            linha.push(c * l)
+        }
+        tabela.push(linha)
+    }
+    return tabela
+}
+console.table(gerarTabela())
+
+
+function gerarTabelaBonus(n){
+    let tabela = []
+    for(let l = 0; l <= n; l++){
+        let linha = []
+        for(let c = 0; c <= n; c++){
+            linha.push(c * l)
+        }
+        tabela.push(linha)
+    }
+    return tabela
+}
+console.table(gerarTabelaBonus(7))
